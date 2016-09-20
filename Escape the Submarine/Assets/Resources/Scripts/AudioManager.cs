d1495e3caca0077505c4aa027f0b6d772e7cd816
Jaswir <j.t.raghoe@students.uu.nl>
2016-09-20 16:55:30 +0200

    Added black background and door triggers

---
+++ Escape the Submarine/Assets/Resources/Scripts/AudioManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Manages Audiocall 
+/// need audio? Call: GameManager.instance._audiManager.Play("name of audio in audio map in resources map");
+/// </summary>
+[RequireComponent(typeof(AudioSource))]
+public class AudioManager : MonoBehaviour
+{
+    public List<AudioObject> Audio_Objects = new List<AudioObject>();
+    public AudioSettings[] audioSettings;
+    private AudioSource _audioSource;
+
+    public static AudioManager Instance;
+
+    public class AudioObject
+    {
+        public string key;
+        public AudioClip audioClip;
+    }
+
+    /// <summary>
+    /// Applies the singleton pattern.
+    /// </summary>
+    private void ApplySingleton()
+    {
+        //Singleton 
+        if (Instance == null)
+            Instance = this;
+
+        if (this != Instance)
+            Destroy(gameObject);
+    }
+    public void Awake()
+    {
+        ApplySingleton();
+        setupAudio();
+    }
+
+    /// <summary>
+    /// Fills audioSettings List from audio's available in the Resources map.
+    /// instantiates _audioSource.
+    /// </summary>
+    void setupAudio()
+    {
+        _audioSource = GetComponent<AudioSource>();
+
+        //Fill audioSettings List from audio's available in the Resources map.
+        foreach (AudioClip audioClip in Resources.LoadAll("Audio"))
+        {
+            //Makes new audio setting
+            AudioObject audioSetting = new AudioObject();
+            audioSetting.key = audioClip.name;
+            audioSetting.audioClip = audioClip;
+
+            //Finally add the setting to the list
+            Audio_Objects.Add(audioSetting);
+        }
+    }
+
+    public void Play(string key)
+    {
+        AudioClip audioClip = Audio_Objects.Find(item => item.key == key).audioClip;
+        _audioSource.clip = audioClip;
+        _audioSource.Play();
+    }
+}
---
+++ Escape the Submarine/Assets/Resources/Scripts/DoorBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class DoorBehaviour : MonoBehaviour
+{
+
+
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        AudioManager.Instance.Play("poepje");
+    }
+}
