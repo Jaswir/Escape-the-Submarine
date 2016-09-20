a523eb77cf5ba3c83449990da32527dada740dd6
Jaswir <j.t.raghoe@students.uu.nl>
2016-09-20 16:35:00 +0200

    Startup for escape the submarine

---
+++ Escape the Submarine/Assets/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody _rb;
+    public float speed = 1;
+    // Use this for initialization
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Debug.Log(moveHorizontal);
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        _rb.velocity = movement * speed;
+
+
+    }
+}
